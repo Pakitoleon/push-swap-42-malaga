c3b477610d8d10669b31475fa65697b0cd44f750
fgonzal2 <pakitoleon@hotmail.es>
2024-05-25 10:42:13 +0200

    new archive

--- push_swap/ft_case.c
+++ push_swap/ft_case.c
@@ -1,29 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ft_case.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgonzal2 <fgonzal2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/22 14:32:21 by @fgonzal2         #+#    #+#             */
-/*   Updated: 2024/05/23 16:42:13 by fgonzal2         ###   ########.fr       */
+/*   Created: 2024/05/23 16:23:35 by fgonzal2          #+#    #+#             */
+/*   Updated: 2024/05/25 10:33:20 by fgonzal2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
-{
-	t_stack	*a;
-
-	a = ft_process(argc, argv);
-	if (!a || ft_checkdup(a))
-	{
-		ft_free(&a);
-		ft_error();
-	}
-	if (!ft_checksorted(a))
-		ft_sort(&a);
-	ft_free(&a);
-	return (0);
-}
--- push_swap/ft_error.c
+++ push_swap/ft_error.c
@@ -1,29 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgonzal2 <fgonzal2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/22 14:32:21 by @fgonzal2         #+#    #+#             */
-/*   Updated: 2024/05/23 16:42:13 by fgonzal2         ###   ########.fr       */
+/*   Created: 2024/05/23 15:32:30 by fgonzal2          #+#    #+#             */
+/*   Updated: 2024/05/23 15:33:30 by fgonzal2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-int	main(int argc, char **argv)
+void	ft_error(void)
 {
-	t_stack	*a;
-
-	a = ft_process(argc, argv);
-	if (!a || ft_checkdup(a))
-	{
-		ft_free(&a);
-		ft_error();
-	}
-	if (!ft_checksorted(a))
-		ft_sort(&a);
-	ft_free(&a);
-	return (0);
+	write(2, "Error\n", 6);
+	exit(1);
 }
